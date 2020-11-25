0cf8a1e3018542f0589353f013b16b45f87ba028
Phil Davis <phildavis17@gmail.com>
2020-11-24 21:52:08 -0500

    some Flask related planning work

--- app/routes.py
+++ app/routes.py
@@ -16,3 +16,27 @@ def base(title):
 #newbirdcity/analysis/001101110/prospect park (birds that are specialties of this park, while on this route)
 #newbirdcity/analysis/11111111111111/prospect park
 #newbirdcity/analysis/prospect park (basline info about prospect park)
+
+@app.route('/analysis')
+def analysis():
+    title = "Hotspot Analysis"
+    return render_template('analysis.html', title=title)
+
+
+@app.route('/analysis/species/<sp_name>')
+def analysis_species(sp_name):
+    pass
+
+
+@app.route('/analysis/hotspots/<hs_name>') # the /hotspots/ part saves me from having to decide whether the thing submitted is a park or a bird
+def analysis_hotspot(hs_name):
+    pass
+
+
+@app.route('/analysis/<rt_index>')
+def hs_route(rt_index):
+    pass
+
+
+# Would I then do something like /analysis/<rt_index>/hotspots/<hotspot>
+# and then                       /analysis/<rt_index>/species/<species>
