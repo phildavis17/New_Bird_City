ecbd28e7e836efccedf9b12b995a0e777d86d113
Phil Davis <phildavis17@gmail.com>
2020-12-19 10:44:54 -0500

    Initial Trip functionality in Flask.

--- app/trip.py
+++ app/trip.py
@@ -7,8 +7,8 @@ import json
 import random
 
 
-MASTER_JSON = R'D:\Douments\Code\New_Bird_City\BK_May_3.json'  # Desktop Version
-# MASTER_JSON = R'C:\Documents\Code\New_Bird_City\BK_May_3.json'  # Laptop Version
+# MASTER_JSON = R'D:\Douments\Code\New_Bird_City\BK_May_3.json'  # Desktop Version
+MASTER_JSON = R'C:\Documents\Code\New_Bird_City\BK_May_3.json'  # Laptop Version
 
 
 def parse_json(file_location):
