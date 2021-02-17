ebad89d079b0a2b38cede119658f8b2cac75b393
Phil Davis <phildavis17@gmail.com>
2021-02-16 22:16:00 -0500

    initial map work.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired, Optional
 
 class MyForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
+    for i in range(10):
+        btest = BooleanField(i, validators=[Optional()])
+
+
+
+#class TripCreationForm(FlaskForm):
