4f1f0280150785e0da08ade80b0287293393136a
Phil Davis <phildavis17@gmail.com>
2021-02-16 23:59:32 -0500

    map troubleshooting.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from config import Config
+from pathlib import Path
 
 app = Flask(__name__)
 app.config.from_object(Config)
--- app/static/mapscript.js
+++ app/static/mapscript.js
@@ -6,5 +6,5 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: '***REMOVED***'
+    accessToken: '***REMOVED***',
 }).addTo(mymap);
