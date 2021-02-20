0d96af4cbbdff009685d5d41aed2b51d10c0b696
Phil Davis <phildavis17@gmail.com>
2021-02-19 22:48:36 -0500

    first stab at an interactive table.

--- app/static/analysisscript.js
+++ app/static/analysisscript.js
@@ -1,3 +1,23 @@
 // Change 0.0 to gray color
 // Change text of values above 0.99 to ">99%"
 
+function toggleActive() {
+    const cbs = document.getElementsByClassName("includeCB");
+    for (let cb of cbs) {
+        let items = document.getElementsByClassName(cb.name);
+        if (cb.checked == true) {
+            for (let i of items) {
+                i.classList.remove("inactive")
+            }
+        } else {
+            for (let i of items) {
+                i.classList.add("inactive")
+            }
+        }
+    }
+}
+
+
+//let chkBxs  = document.querySelectorAll('input[class=includeCB]');
+
+
